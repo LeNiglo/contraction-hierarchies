6f4a79c6e444a9d461981bd46d88f5372b0ba43c
Adrien Della Maggiora <dellam_a@epitech.eu>
2016-12-02 14:51:23 +0100

    End step 3

--- etape 3/base.h
+++ etape 3/base.h
@@ -57,11 +57,18 @@ class FatalLogger : public Logger {
                         << "Right value: " << (y) << std::endl
 
 #define CHECK_EQ(x, y) CHECK_BINARY(==, x, y)
+#define CHECK_EQ(x, y) CHECK_BINARY(==, x, y)
 #define CHECK_NE(x, y) CHECK_BINARY(!=, x, y)
 #define CHECK_LT(x, y) CHECK_BINARY(<, x, y)
 #define CHECK_GT(x, y) CHECK_BINARY(>, x, y)
 #define CHECK_LE(x, y) CHECK_BINARY(<=, x, y)
 #define CHECK_GE(x, y) CHECK_BINARY(>=, x, y)
+
+#define CHECK_NEAR(x, y, eps) \
+    CHECK(std::abs(double(x) - double(y)) <= double(eps))\
+        << "Left value: " << (x) << std::endl\
+        << "Right value: " << (y) << std::endl\
+        << "Diff: " << std::endl
 
 #define LOG Logger(__FILE__, __LINE__).stream()
 
@@ -78,9 +85,11 @@ template<class T>
 string PrintHistogram(const T& v) {
   typedef typename T::value_type Value;
   map<Value, int> count;
-  for (const Value& x : v) ++count[x];
-  vector<pair<int, Value>> pairs;
-  for (const auto& p : count) pairs.push_back({p.second, p.first});
+  for (typename T::const_iterator it = v.begin(); it != v.end(); ++it) ++count[*it];
+  vector<pair<int, Value> > pairs;
+  for (typename map<Value, int>::const_iterator it = count.begin(); it != count.end(); ++it) {
+    pairs.push_back(std::make_pair(it->second, it->first));
+  }
   std::sort(pairs.begin(), pairs.end());
   std::reverse(pairs.begin(), pairs.end());
   std::ostringstream os;
@@ -94,7 +103,7 @@ template<class T>
 string PrintList(const T& v) {
   std::ostringstream os;
   os << "[ ";
-  for (const auto x : v) os << x << " ";
+  for (typename T::const_iterator it = v.begin(); it != v.end(); ++it) os << *it << " ";
   os << "]";
   return os.str();
 }
