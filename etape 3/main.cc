389d3f1732a18c85e4e6fd5d6dc15f570621a261
Guillaume Lefrant <lefrantguillaume@gmail.com>
2016-12-02 17:49:55 +0100

    prepare merge

--- etape 3/main.cc
+++ etape 3/main.cc
@@ -26,6 +26,36 @@ std::pair<std::pair<double, double>, std::pair<double, double> >	parseLine(std::
 	);
 }
 
+void processLine(RoadData &data, std::string &line)
+{
+	std::vector<int> nodes;
+	double distance;
+	std::pair<std::pair<double, double>, std::pair<double, double> > query = parseLine(line);
+	int node = findNodeAt(data, query.first);
+	int node2 = findNodeAt(data, query.second);
+
+	if (node != -1 && node2 != -1)
+	{
+		nodes.push_back(findNodeAt(data, query.second));
+		Dijkstra dijkstra(&data.graph, &data.arc_durations);
+		dijkstra.RunUntilAllTargetsAreReached(node, nodes);
+
+		if ((distance = dijkstra.Distances()[nodes[0]]) == std::numeric_limits<double>::infinity())
+		{
+			std::cout << "NO PATH" << std::endl;
+		}
+		else
+		{
+			std::cout << distance << std::endl;
+		}
+
+	}
+	else
+	{
+		std::cout << "INVALID" << std::endl;
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc != 2)
 	{
@@ -34,30 +64,9 @@ int main(int argc, char** argv) {
 	RoadData data = ParseCsvFile(argv[1]);
 
 	std::string line;
-	while (std::cin >> line){
-		std::vector<int> nodes;
-		int node, node2;
-
-		std::pair<std::pair<double, double>, std::pair<double, double> > query = parseLine(line);
-		fprintf(stderr, "[%lf , %lf]  =>  [%lf , %lf]\n", query.first.first, query.first.second, query.second.first, query.second.second);
-		node = findNodeAt(data, query.first);
-		node2 = findNodeAt(data, query.second);
-
-		fprintf(stderr, "%d // %d\n", node, node2);
-		if (node != -1 && node2 != -1)
-		{
-			nodes.push_back(findNodeAt(data, query.second));
-			Dijkstra dijkstra(&data.graph, &data.arc_durations);
-			dijkstra.RunUntilAllTargetsAreReached(node, nodes);
-
-			// if ((distance = dijkstra.getDistance()) == infinity) => "NO PATH"
-
-		}
-		else
-		{
-			std::cout << "INVALID" << std::endl;
-		}
-
+	while (std::getline(cin, line)){
+		processLine(data, line);
+		line = "";
 	}
 
 	return (EXIT_SUCCESS);
